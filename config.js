37778048eaa924073d8471a2927d7f4f12a0e324
lucionathan <lucio.matias@ccc.ufcg.edu.br>
2020-02-06 16:50:40 -0300

    token infeliz

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
     
     consumer_secret:'ok9e3I95yAIlMrsb3y8fLvBFIxpEcb6FX3aTK3ZrOKHE9QdSFn',
     
-    access_token:'1223278586653749250-uUWObNmKM9LdBKk6JH0zaWTuK82fnx',
+    access_token:'1223278586653749250-6nlXCS7XVMWJNtgKWYNeDcRE5ZMqvW',
     
-    access_token_secret:'QWxrAlHCgItYGilISPSxz9n79j99lmKbWulX6QZDZXW53'
+    access_token_secret:'ftuxND8j9uFl4cuZ25mjcs29eiAvkquq9qjXBZVrjStSU'
     
 }
--- index.js
+++ index.js
@@ -4,3 +4,10 @@ var request = require("request");
 var secret = require("./config");
 
 var Twitter = new TwitterPackage(secret);
+
+Twitter.get('search/tweets', {q: '#teste', result_type:'recent'}, function(error, tweets, response) {
+    console.log(tweets);
+    tweets.statuses.forEach(function(tweet) {
+        console.log("tweet: " + tweet.text);
+    });
+ });
