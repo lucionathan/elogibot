5e4e8e48f76a375075d03c4e924f8512a05dbde3
lucionathan <lucio.matias@ccc.ufcg.edu.br>
2020-02-07 18:54:34 -0300

    aparentemente funcionando

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
 
-    consumer_key:'FeyiIRTyLVKufUGyudy9cXWsW',
+    consumer_key:'KLnSOnCbF5cP8WSwnuzwYIK8i',
     
-    consumer_secret:'dfIIdt5Opk6bBmq1BCcoYfl8Isd9KtYzYxCIrZLK90A5N3Dgmk',
+    consumer_secret:'ok9e3I95yAIlMrsb3y8fLvBFIxpEcb6FX3aTK3ZrOKHE9QdSFn',
     
-    access_token_key:'1223278586653749250-2kNvTxSz7nnEpblWajCis6rAbuPxgc',
+    access_token_key:'1223278586653749250-CNwSGmTB7B3gbsx8GkjTiJqW8jYp2j',
     
-    access_token_secret:'SHiVlr1nUZIVGjihGNvyFWa0d4zboKUvEbEg19Oo50sCt'
+    access_token_secret:'09mssUaAquQbeNJcF5Plij1khrlF6Rn9EK7edtYvtmGBl'
     
 }
--- index.js
+++ index.js
@@ -1,18 +1,72 @@
 var TwitterPackage = require("twitter");
-var request = require("request");
 
 var secret = require("./config");
 
+var elogios = require('./elogios.json'); 
+
+var Tweets = require('./tweets.json').tweets;
+
 var Twitter = new TwitterPackage(secret);
 
-Twitter.stream('statuses/filter', { track: 'paranaue' }, function (stream) {
-    stream.on('data', function (tweet) {
-        console.log('Stream: ', tweet.id, '\n', tweet.id_str)
-        Twitter.post('statuses/update', { status: 'parana', in_reply_to_status_id: tweet.id_str, auto_populate_reply_metadata: true }, function (error, tweet, response) {
-            if (error) console.log(error);
-            console.log('Tweet ',tweet);  // Tweet body.
+
+function classificaString(tweet){ 
+
+    var retorno = "";
+    var substrings = ["feia", "feio", "burra", "burro", "insuficiente"]
+    tweet.split(' ').forEach(element => {
+        substrings.forEach(substring => {
+            if(substring == element) {
+                retorno = element;
+            }
         });
-// var randomAccomplishment  = array[Math.floor(Math.random() * array.length)];
+    });
+
+    return retorno;
+}
+
+
+function getElogio(elogios, classificacao) {
+
+    var retorno = "";
+
+    switch (classificacao) {
+        case "feio":
+            retorno = elogios.masculino.aparencia[Math.floor(Math.random() * elogios.masculino.aparencia.length)];
+            break;
+    
+        case "burro":
+            retorno = elogios.masculino.inteligencia[Math.floor(Math.random() * elogios.masculino.inteligencia.length)];
+            break;
+   
+        case "feia":
+            retorno = elogios.feminino.aparencia[Math.floor(Math.random() * elogios.masculino.aparencia.length)];
+            break;
+            
+        case "burra":
+            retorno = elogios.feminino.inteligencia[Math.floor(Math.random() * elogios.masculino.inteligencia.length)];
+            break;
+            
+        case "insuficiente":
+            retorno = elogios.insuficiente[Math.floor(Math.random() * elogios.insuficiente.length)];
+            break;
+                }
+    
+    return retorno;
+
+}
+
+Twitter.stream('statuses/filter', { track: Tweets}, function (stream) {
+    stream.on('data', function (tweet) {
+        if(tweet.retweeted_status == undefined && !tweet.is_quote_status && tweet.in_reply_to_status_id == null) {
+            console.log(tweet.text);
+
+            var elogio = getElogio(elogios, classificaString(tweet.text));
+
+            Twitter.post('statuses/update', { status: elogio, in_reply_to_status_id: tweet.id_str, auto_populate_reply_metadata: true }, function (error, tweet, response) {
+                if (error) console.log(error);
+            });
+        }
+
     });
 
     stream.on('error', function (error) {
